5f4a7f67f88ef556b824a1be4abf474b4392902b
scardummichele <scarpa.michele.90@gmail.com>
2022-06-04 15:12:48 +0200

    get mock

--- apps/jm-site/src/App.svelte
+++ apps/jm-site/src/App.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+import ResumeMockPage from './pages/ResumeMockPage.svelte';
+
   import Tailwind from './Tailwind.svelte';
   export let name: string;
 </script>
@@ -6,11 +8,8 @@
 <Tailwind />
 
 <main>
-  <h1 class="bg-pink-500 text-gray-100 font-bold text-xl rounded-lg px-5">
-    Hello {name} from the <span class="text-7xl">Svelte</span> Tailwind Nx template!
-  </h1>
-  <div>
-    <span class="mt-4 text-red-500 text-5xl font-bold">Example goes here</span>
-  </div>
+
+  <ResumeMockPage />
+
 </main>
 
