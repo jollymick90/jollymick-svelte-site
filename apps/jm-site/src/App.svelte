e8d3bd95c7edc570b89518135f7da5d715422f26
scardummichele <scarpa.michele.90@gmail.com>
2022-06-04 15:49:41 +0200

    fail

--- apps/jm-site/src/App.svelte
+++ apps/jm-site/src/App.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
-import ResumeMockPage from './pages/ResumeMockPage.svelte';
 
   import Tailwind from './Tailwind.svelte';
-  export let name: string;
 </script>
 
 <Tailwind />
 
 <main>
-
-  <ResumeMockPage />
-
+ciao
 </main>
-
---
+++ apps/jm-site/src/components/JMRouter.svelte
@@ -0,0 +1,27 @@
+<!-- <script lang="ts">
+    import { Router, Link, Route } from 'svelte-routing';
+    import LandingMockPage from './pages/LandingMockPage.svelte';
+    import PortalMockPage from './pages/PortalMockPage.svelte';
+  
+    import Tailwind from './Tailwind.svelte';
+    export let url = '';
+  </script>
+  
+  
+  <main>
+    <Router {url}>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="landing">Landing</Link>
+        <Link to="portal">Portal</Link>
+      </nav>
+      <div>
+        <Route path="portal" component={LandingMockPage} />
+        <Route path="landing"><PortalMockPage /></Route>
+        <Route path="/">
+          <div>ciao</div>
+        </Route>
+      </div>
+    </Router>
+  </main>
+   -->
